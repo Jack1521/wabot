cd9f2c878c6dff3b530896ea7e209f9cba703039
Jack <94463439+Jack1521@users.noreply.github.com>
2022-01-08 11:30:28 +0330

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ global.thumb = fs.readFileSync('./src/thumb.jpeg')
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
 global.owner = ['6289681378568', '6283820073017', '6283801499848','85290882526','6285240750713','6288286421519','62877745695315'] // Put your number here
 global.mods = ['6285693120588'] // Want some help?
-global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.prems = ['6287842147162','989035024059','6285656615441'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
